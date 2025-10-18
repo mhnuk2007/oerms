5dc1c8ece0063eeec2a4734d25f83ba0673d86f8
Mohan Lal <mhnuk2007@gmail.com>
2025-10-18 22:25:14 +0500

    Add api-gateway microservice

---
+++ oerms-backend/api-gateway/src/main/java/com/oerms/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.oerms.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Main entry point for the API Gateway service.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
